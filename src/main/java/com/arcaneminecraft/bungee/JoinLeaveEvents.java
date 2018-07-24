c143a7861f1f15019e6ff64bca9244ddcc75c558
Simon Chuu <simonorj@outlook.com>
2018-07-24 04:18:28 -0400

    Add donor messages to welcome

--- src/main/java/com/arcaneminecraft/bungee/JoinLeaveEvents.java
+++ src/main/java/com/arcaneminecraft/bungee/JoinLeaveEvents.java
@@ -16,13 +16,43 @@ import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
 import java.text.DateFormat;
+import java.util.Random;
 import java.util.TimeZone;
 import java.util.concurrent.TimeUnit;
 
 public class JoinLeaveEvents implements Listener {
     private final ArcaneBungee plugin;
     private final BaseComponent welcomeMessage;
-
+    private static final String[] DONOR = {
+            "You're pretty awesome. Seriously.",
+            "You're pretty awesome. Seriously. 100% awesome.",
+            "By donating, you've helped make Arcane possible. " + ChatColor.BOLD + "Thanks!",
+            "Welcome back to Arcane.",
+            "You're way cooler than everybody else.",
+            "Thank you for your support!",
+            "You should make use of your powerful /slap command.",
+            "Thank you!",
+            "Thank you for supporting Arcane!",
+            "Don't forget, you have access to /slap. Use it wisely.",
+            "You can hide yourself from the Dynmap via /dynmap hide.",
+            "Did you know there's a donor only section on the forums?",
+            "Welcome back to Arcane Survival!",
+            "We love you.",
+            "We love you. A lot.",
+            "Tip: To reappear from the Dynmap if you've hidden yourself, type /dynmap show.",
+            "Thank you.",
+            "If you'd like to hide yourself from our Dynmap, type /dynmap hide.",
+            "Did we tell you you're awesome? You really are.",
+            //"You're pretty awesome. Not as awesome as Agentred100 is, though.", // <.<
+            //"You're pretty awesome. Almost as awesome as _NickV, keep it up.",  // >.>
+            "Thank you. You're awesome.",
+            "Welcome back to Arcane Survival.",
+            "You're a pretty cool person.",
+            "What cool stuff can we give to our donors? Let us know on the forums.",
+            "We appreciate your support.",
+            "If you're looking for some building ideas, you can type /dclem.",
+            "We appreciate your support.",
+    };
 
     JoinLeaveEvents(ArcaneBungee plugin) {
         this.plugin = plugin;
@@ -67,6 +97,11 @@ public class JoinLeaveEvents implements Listener {
         this.welcomeMessage.addExtra("\n");
     }
 
+    private static String getRandomDonorMsg() {
+        Random rand = new Random();
+        return DONOR[rand.nextInt(DONOR.length)];
+    }
+
     @EventHandler
     public void onLoginJoin(PostLoginEvent e) {
         if (plugin.getSqlDatabase() != null) {
@@ -77,7 +112,16 @@ public class JoinLeaveEvents implements Listener {
                 }
 
                 if (p.hasPermission("arcane.welcome.donor") && OptionsStorage.get(p, OptionsStorage.Toggles.SHOW_DONOR_WELCOME_MESSAGE)) { // TODO: Check permission node existence on ArcaneServer
-                    p.sendMessage(ChatMessageType.SYSTEM, new TextComponent("You are a donor! This message is still a work in progress"));
+                    BaseComponent send = new TextComponent(" ");
+                    send.setColor(ArcaneColor.CONTENT);
+                    BaseComponent urad = new TextComponent("You are a donor!");
+                    urad.setColor(ArcaneColor.DONOR);
+                    send.addExtra(urad);
+
+                    for (BaseComponent bp : TextComponent.fromLegacyText(getRandomDonorMsg()))
+                        send.addExtra(bp);
+
+                    p.sendMessage(ChatMessageType.SYSTEM, send);
                 }
 
                 if (time != null && OptionsStorage.get(p, OptionsStorage.Toggles.SHOW_LAST_LOGIN_MESSAGE)) {

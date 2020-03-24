9b86c41e691d0f7f2df70705ba067c187a8ee6af
Simon Chuu <simonorj@outlook.com>
2020-03-23 22:49:44 -0400

    Roll command is still WIP, but someone should finish it up

--- src/main/java/com/arcaneminecraft/bungee/command/RollCommand.java
+++ src/main/java/com/arcaneminecraft/bungee/command/RollCommand.java
@@ -2,7 +2,10 @@ package com.arcaneminecraft.bungee.command;
 
 import com.arcaneminecraft.api.BungeeCommandUsage;
 import com.arcaneminecraft.bungee.DiscordCommandExecutor;
+import net.dv8tion.jda.core.entities.Message;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.plugin.Command;
 import net.md_5.bungee.api.plugin.TabExecutor;
 
@@ -23,13 +26,26 @@ public class RollCommand extends Command implements TabExecutor, DiscordCommandE
     }
 
     private List<Integer> rolled(int repeat, int max) {
-        List<Integer> ret = new ArrayList<>(repeat);
+        //List<Integer> ret = new ArrayList<>(repeat);
         // TODO
+        return null;
     }
 
     @Override
     public void execute(CommandSender sender, String[] args) {
+        sender.sendMessage(TextComponent.fromLegacyText("Not implemented yet"));
         // TODO
+    }
+
+    @Override
+    public boolean executeDiscordCommand(Message trigger, String[] args) {
+        trigger.getChannel().sendMessage("Not implemented yet").complete();
+        return false;
+    }
+
+    @Override
+    public Iterable<String> onTabComplete(CommandSender sender, String[] args) {
+        return null;
     }
 
     // TODO make discord executor
